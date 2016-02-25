a762250037406ddf00637c5532c1f8b267015804
Passyt <passyt@gmail.com>
2016-02-25 21:04:31 +0800

    Change package name

--- src/test/java/com/derby/nuke/DefaultApplicationTests.groovy
+++ src/test/java/com/derby/nuke/DefaultApplicationTests.groovy
@@ -2,12 +2,9 @@ package com.derby.nuke
 
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.springframework.test.context.web.WebAppConfiguration
-
-import springboot.webchat.WebchatApplication;
-
 import org.springframework.boot.test.SpringApplicationConfiguration
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+import org.springframework.test.context.web.WebAppConfiguration
 
 @RunWith(SpringJUnit4ClassRunner)
 @SpringApplicationConfiguration(classes = DefaultApplication)
