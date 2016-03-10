6331264efd8d99ef2de536f2efe294c78aa408aa
Passyt <passyt@gmail.com>
2016-03-10 13:06:52 +0800

    Disable cache in browser

--- src/main/java/com/derby/nuke/wheatgrass/wechat/LoginInterceptor.groovy
+++ src/main/java/com/derby/nuke/wheatgrass/wechat/LoginInterceptor.groovy
@@ -56,6 +56,10 @@ class LoginInterceptor extends HandlerInterceptorAdapter  {
 				}
 			}
 		}
+		response.setHeader("Cache-Control", "no-cache");
+        response.setHeader("Cache-Control", "no-store");
+        response.setDateHeader("Expires", 0);
+        response.setHeader("Pragma", "no-cache");
 		return true;
 	}
 }
