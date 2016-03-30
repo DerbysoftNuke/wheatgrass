88cd1d1f210a7c9c1ae0a672cee912ac9f1015a4
Passyt <passyt@gmail.com>
2016-03-30 13:45:57 +0800

    add welcome message while register

--- src/main/java/com/derby/nuke/wheatgrass/wechat/controller/RegisterController.groovy
+++ src/main/java/com/derby/nuke/wheatgrass/wechat/controller/RegisterController.groovy
@@ -1,9 +1,7 @@
 package com.derby.nuke.wheatgrass.wechat.controller;
 
-import java.time.LocalDate
-
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RequestParam
@@ -11,11 +9,9 @@ import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.servlet.ModelAndView
 
 import com.derby.nuke.wheatgrass.ScheduleConfiguration
-import com.derby.nuke.wheatgrass.entity.Sex
-import com.derby.nuke.wheatgrass.entity.User
-import com.derby.nuke.wheatgrass.repository.UserRepository
 import com.derby.nuke.wheatgrass.wechat.OAuthRequired
-import com.derby.nuke.wheatgrass.wechat.WechatException;
+import com.derby.nuke.wheatgrass.wechat.WechatException
+import com.fasterxml.jackson.databind.ObjectMapper
 
 @RestController
 @OAuthRequired(false)
@@ -23,9 +19,9 @@ class RegisterController extends WechatController{
 	
 	@Autowired
 	def ScheduleConfiguration scheduleConfiguration;
-	@Value("wechat.welcome.message.content")
+	@Value('${wechat.welcome.message.content}')
 	def String welcomeMessageContent;
-	@Value("wechat.welcome.message.type")
+	@Value('${wechat.welcome.message.type}')
 	def String welcomeMessageType;
 
 	@RequestMapping(value="/register", method = RequestMethod.GET)
@@ -33,7 +29,8 @@ class RegisterController extends WechatController{
 		def userId = wechatService.getUserId(code);
 		try{
 			wechatService.register(userId);
-			wechatService.sendMessage(userId, welcomeMessageType, welcomeMessageContent);
+			def message = new ObjectMapper().readValue(welcomeMessageContent, Map.class);
+			wechatService.sendMessage(userId, welcomeMessageType, message);
 		}catch(WechatException e){
 			if(e.errorCode != "50004"){
 				throw e;
