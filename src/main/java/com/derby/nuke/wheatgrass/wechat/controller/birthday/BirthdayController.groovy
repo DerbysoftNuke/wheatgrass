04717adfb7103cda692882fceace1749b6a5c9f9
caochengkai <caochengkai@10.200.152.64>
2016-06-27 11:22:02 +0800

    ...

--- src/main/java/com/derby/nuke/wheatgrass/wechat/controller/birthday/BirthdayController.groovy
+++ src/main/java/com/derby/nuke/wheatgrass/wechat/controller/birthday/BirthdayController.groovy
@@ -64,7 +64,7 @@ class BirthdayController extends WechatController{
 		
 		def entryday = birthdayWish.user.entryday;
 		if(entryday != null){
-			def days = ChronoUnit.DAYS.between(entryday, birthday);
+			def days = ChronoUnit.DAYS.between(entryday, LocalDate.now());
 			params["entrydays"] = days;
 		}
 		return view("show_wish",params);
