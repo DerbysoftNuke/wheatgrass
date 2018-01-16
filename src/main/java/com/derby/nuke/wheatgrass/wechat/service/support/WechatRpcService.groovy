15a65da0a77287f44e10a52c202be93aadbdd79f
Eric Kuang <kck@derbysoft.com>
2018-01-16 12:18:36 +0800

    fix bug

--- src/main/java/com/derby/nuke/wheatgrass/wechat/service/support/WechatRpcService.groovy
+++ src/main/java/com/derby/nuke/wheatgrass/wechat/service/support/WechatRpcService.groovy
@@ -1,5 +1,13 @@
 package com.derby.nuke.wheatgrass.wechat.service.support
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import com.fasterxml.jackson.databind.deser.std.CollectionDeserializer
+import com.fasterxml.jackson.databind.deser.std.MapDeserializer
+import com.fasterxml.jackson.databind.deser.std.StringDeserializer
+import com.fasterxml.jackson.databind.ser.std.CollectionSerializer
+import com.fasterxml.jackson.databind.ser.std.MapSerializer
+import com.fasterxml.jackson.databind.ser.std.StringSerializer
 import com.googlecode.jsonrpc4j.JsonRpcMethod
 import com.googlecode.jsonrpc4j.JsonRpcService
 
@@ -10,6 +18,10 @@ import com.googlecode.jsonrpc4j.JsonRpcService
 interface WechatRpcService {
 
     @JsonRpcMethod("sendMessage")
-    def sendMessage(userIds, type, message, agentId)
+    def sendMessage(
+            @JsonSerialize(using = CollectionSerializer) @JsonDeserialize(using = CollectionDeserializer) userIds,
+            @JsonSerialize(using = StringSerializer) @JsonDeserialize(using = StringDeserializer) type,
+            @JsonSerialize(using = MapSerializer) @JsonDeserialize(using = MapDeserializer) message,
+            @JsonSerialize(using = StringSerializer) @JsonDeserialize(using = StringDeserializer) agentId)
 
 }
